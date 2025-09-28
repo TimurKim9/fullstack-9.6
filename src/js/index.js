5866f0364296d67bf4fc5832a50c161aef36fb22
Tim <thetimkim05@gmail.com>
2025-09-28 16:45:47 +0900

    fix services-btn

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 import '../scss/style.scss'
 
 console.log('It works!')
+
+const buttons = document.querySelectorAll('.services-btn button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Сбрасываем класс active у всех кнопок
+    buttons.forEach(btn => btn.classList.remove('active'));
+    
+    // Добавляем active только к нажатой кнопке
+    button.classList.add('active');
+  });
+});
