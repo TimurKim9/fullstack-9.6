9ff1f32fe95a03a6db67e8fa65bfa27f7bdf1bbf
Tim <thetimkim05@gmail.com>
2025-10-05 03:58:06 +0900

    fix servis

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,26 @@ buttons.forEach(button => {
     button.classList.add('active');
   });
 });
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+const toggleBtn = document.getElementById("toggleBtn");
+const toggleText = toggleBtn.querySelector("span");
+const hiddenBtns = document.querySelector(".servis");
+
+toggleBtn.addEventListener("click", () => {
+  hiddenBtns.classList.toggle("show");
+  toggleBtn.classList.toggle("rotated");
+
+  toggleText.textContent = hiddenBtns.classList.contains("show")
+    ? "Скрыть"
+    : "Показать ещё";
+});
