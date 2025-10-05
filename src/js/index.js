54e6824ea65aca8af34d48af149977535065402d
Tim <thetimkim05@gmail.com>
2025-10-05 17:50:59 +0900

    fix brands

--- src/js/index.js
+++ src/js/index.js
@@ -14,25 +14,52 @@ buttons.forEach(button => {
   });
 });
 
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: "auto",
-  spaceBetween: 16,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+// const swiper = new Swiper(".mySwiper", {
+//   slidesPerView: "auto",
+//   spaceBetween: 16,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
 
 
-const toggleBtn = document.getElementById("toggleBtn");
-const toggleText = toggleBtn.querySelector("span");
-const hiddenBtns = document.querySelector(".servis");
+// const toggleBtn = document.getElementById("toggleBtn");
+// const toggleText = toggleBtn.querySelector("span");
+// const hiddenBtns = document.querySelector(".servis");
 
-toggleBtn.addEventListener("click", () => {
-  hiddenBtns.classList.toggle("show");
-  toggleBtn.classList.toggle("rotated");
+// toggleBtn.addEventListener("click", () => {
+//   hiddenBtns.classList.toggle("show");
+//   toggleBtn.classList.toggle("rotated");
 
-  toggleText.textContent = hiddenBtns.classList.contains("show")
-    ? "Скрыть"
-    : "Показать ещё";
-});
+//   toggleText.textContent = hiddenBtns.classList.contains("show")
+//     ? "Скрыть"
+//     : "Показать ещё";
+// });
+
+// Инициализация слайдера
+document.addEventListener("DOMContentLoaded", () => {
+    const swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      pagination: {
+        el: ".brands-slider__pagination",
+        clickable: true,
+      },
+    });
+
+    const toggleBtn = document.getElementById("toggleBtn");
+    const toggleText = toggleBtn.querySelector(".brands__toggle-text");
+    const brandsList = document.querySelector(".brands__list");
+
+    toggleBtn.addEventListener("click", () => {
+      brandsList.classList.toggle("show");
+      toggleBtn.classList.toggle("rotated");
+
+      toggleText.textContent = brandsList.classList.contains("show")
+        ? "Скрыть"
+        : "Показать ещё";
+
+      setTimeout(() => swiper.update(), 100);
+    });
+  });
